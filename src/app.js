de31da3bb323bbeaecfe341daff078a27240dc6f
Michael T Wolfgang <149036@carmax.com>
2022-02-25 16:44:04 -0500

    added app.js

--- src/app.js
+++ src/app.js
@@ -3,4 +3,10 @@ const express = require('express');
 const app = express();
 
 
-app.get('/');
+app.get('/', (req, res) => {
+  res.send('Hello from my app');
+});
+
+app.listen(3000, ()=>{
+    console.log('listening on port 3000');
+});
