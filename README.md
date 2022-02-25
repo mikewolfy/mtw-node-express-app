def512d30a3db713b2c2e2f75d5077a9e781bcf9
Michael T Wolfgang <149036@carmax.com>
2022-02-25 17:11:32 -0500

    Setup initial code
    morgan, debug, static page

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # mtw-node-express-app
+
+```
+npm install express
+```
+
+
+```
+> npm install
+> node app.js
+```
+
+```
+npm install chalk@4.1.1
+```
--- src/app.js
+++ src/app.js
@@ -1,12 +1,20 @@
 const express = require('express');
+const {chalk} = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+const path = require('path');
 
 const app = express();
 
+app.use(morgan('tiny'));  //tiny, combined
+
+app.use(express.static(path.join(__dirname, '/public/')));
 
 app.get('/', (req, res) => {
   res.send('Hello from my app');
 });
 
-app.listen(3000, ()=>{
-    console.log('listening on port 3000');
+app.listen(3000, () => {
+    debug(`listening on port 3000`);  //' + chalk.green('3000'));
+    //console.log(`listening on port ${chalky.chgreen('3000')}`);
 });
